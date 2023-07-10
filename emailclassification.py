77f3dde5c1cb998ec79d552a5d9d9a9e7acfe5ee
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-10 18:51:45 +0800

    commit

--- emailclassification.py
+++ emailclassification.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import nltk
@@ -98,8 +98,8 @@ def main():
     # Split the data into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train a Random Forest classifier
-    classifier = RandomForestClassifier()
+    # Train an AdaBoost classifier
+    classifier = AdaBoostClassifier()
     classifier.fit(X_train, y_train)
 
     # User input
